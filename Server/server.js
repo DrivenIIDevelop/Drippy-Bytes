7003b0e341e0a51c7c35de17fcc781dab8aa5331
Chase Kliment <stepintoliquidd@gmail.com>
2024-04-14 17:58:16 -0600

    installed multer and created our /upload route and a directory in our root directory for handeling file upload and file download

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const upload = require('./config/multerConfig');
+const path = require('path');
+
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
@@ -14,6 +17,11 @@ app.use('/projects', projectRoutes);
 app.use('/projects/:projectId/tasks', taskRoutes); // Correct mounting for task routes
 app.use('/users', userRoutes);
 
+// Route to handle file uploads using Multer
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('File uploaded successfully');
+});
+
 // Define a simple route to test
 app.get('/', (req, res) => {
   res.send('Hello, ProjectPulse!');
@@ -24,4 +32,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
